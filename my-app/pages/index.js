db4aaf4e6cef835cfc675c41fefbe639ea6c5e58
William Cadiz <wcadz27@gmail.com>
2022-03-03 07:19:49 -0800

    Eslint

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -146,7 +146,7 @@ export default function Home() {
         <div>
           <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
           <div className={styles.description}>
-            It's an NFT collection for developers in Crypto.
+            It&apos;s an NFT collection for developers in Crypto.
           </div>
           <div className={styles.description}>
             {numberOfWhitelisted} have already joined the Whitelist
